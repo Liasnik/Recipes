e351212683c5874afa76ca56d3b970e9b1a4a077
Liasnik <9426247@gmail.com>
2024-02-09 00:27:10 +0200

    added Nav-Link.tsx, usePathname, clsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,34 +1,15 @@
-import Link from "next/link";
+import NavLinks from "./Nav-Links";
 
 const Header = () => {
   return (
     <div className="bg-slate-900 py-4 sticky top-0 w-full">
       <div className="mx-auto px-20">
-        <nav className="flex items-center justify-between">
+        <section className="flex items-center justify-between">
           <div className="text-white text-2xl font-bold">{/* Logo */}</div>
-          <ul className="flex space-x-4 md:space-x-7">
-            <li>
-              <Link href="/" className="hover:text-blue-300">
-                Recipes
-              </Link>
-            </li>
-            <li>
-              <Link href="/quotes" className="hover:text-blue-300">
-                Quotes
-              </Link>
-            </li>
-            <li>
-              <Link href="/carts" className="hover:text-blue-300">
-                Carts
-              </Link>
-            </li>
-            <li>
-              <Link href="/posts" className="hover:text-blue-300">
-                Posts
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          <nav className="flex space-x-4 md:space-x-7">
+            <NavLinks />
+          </nav>
+        </section>
       </div>
     </div>
   );
---
+++ app/components/Nav-Links.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+const links = [
+  { name: "Recipes", href: "/" },
+  {
+    name: "Quotes",
+    href: "/quotes",
+  },
+  { name: "Carts", href: "/carts" },
+  { name: "Posts", href: "/posts" },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.name}
+          href={link.href}
+          className={clsx("hover:text-blue-300", {
+            " text-blue-300": pathname === link.href,
+          })}
+        >
+          <p className="hidden md:block">{link.name}</p>
+        </Link>
+      ))}
+    </>
+  );
+}
